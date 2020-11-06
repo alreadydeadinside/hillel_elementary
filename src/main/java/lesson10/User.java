75fed22febfb740ef44f8510bfbe1c6d9317fdc2
BoleanSwift <brytong@ukr.net>
2020-11-06 20:05:09 +0200

    create test java class

---
+++ src/main/java/lesson10/User.java
@@ -0,0 +1,39 @@
+package lesson10;
+
+import java.util.Objects;
+
+public class User {
+    private String Name;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Name='" + Name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(Name, user.Name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Name);
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public User(String name) {
+        Name = name;
+    }
+}
