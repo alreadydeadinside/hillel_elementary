f855237ce1c8e9113aa46b3dc4911d48f3d8f2a2
BoleanSwift <brytong@ukr.net>
2020-12-11 20:01:55 +0200

    add logging files

--- src/main/java/lesson20/Main.java
+++ src/main/java/lesson20/Main.java
@@ -1,2 +1,14 @@
-package lesson20;public class Main {
+package lesson20;
+
+import lesson20.utils.LoggerUtil;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+   static Logger logger = LoggerUtil.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.log(Level.INFO, "Some Error");
+       // System.err.println("Error message from lesson 20");
+    }
 }
--- src/main/java/lesson20/utils/LoggerUtil.java
+++ src/main/java/lesson20/utils/LoggerUtil.java
@@ -1,2 +1,13 @@
-package lesson20.utils;public class LoggerUtil {
+package lesson20.utils;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class LoggerUtil {
+    public static <T>Logger getLogger(Class<T> tClass){
+        try{
+            LogManager.getLogManager().readConfiguration(tClass.getResourceAsStream("log/"));
+        }
+
+    }
 }
