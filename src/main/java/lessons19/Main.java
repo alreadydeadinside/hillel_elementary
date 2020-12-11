a1c425fdce625b821f3757378a8e11ef612274c5
BoleanSwift <brytong@ukr.net>
2020-12-11 20:02:19 +0200

    add other unnecessary files

--- src/main/java/lessons19/Main.java
+++ src/main/java/lessons19/Main.java
@@ -1,2 +1,7 @@
-package lessons19;public class Main {
+package lessons19;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/lessons19/WelcomeServlet.java
+++ src/main/java/lessons19/WelcomeServlet.java
@@ -1,2 +1,15 @@
-package lessons19;public class WelcomeServlet {
+package lessons19;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class WelcomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("welcome.jsp").forward(req, resp);
+
+    }
 }
