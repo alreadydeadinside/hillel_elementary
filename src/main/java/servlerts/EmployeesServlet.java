931d91d954274250f2c84bd7a88c951349fafb11
Bohdan Vakaliuk <54852515+BoleanSwift@users.noreply.github.com>
2020-12-12 01:46:05 +0200

    add servlet for any request

---
+++ src/main/java/servlerts/EmployeesServlet.java
@@ -0,0 +1,31 @@
+package servlerts;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Bohdan Vakaliuk
+ * @version 12.12.2020
+ */
+
+//TODO
+// 1. Создать 4 Сервлета и 4 JSP страницы связанных с ними;
+// 2. 2 пары связать с помощью конфигурации web.xml;
+// 3. 2 пары связать с помощью аннотаций;
+// 4. На каждой странице сделать ссылку на другую страницу.
+// 5. Наполнение страниц не имеет значения. Какая страница какую ссылку будет иметь также не имеет значения, главное чтобы не было ситуации, когда перешел на jsp и из нее не было ссылки на другую (тупик)
+
+
+@WebServlet("/customers")
+public class EmployeesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("Customers.jsp").forward(req, resp);
+    }
+}
+
